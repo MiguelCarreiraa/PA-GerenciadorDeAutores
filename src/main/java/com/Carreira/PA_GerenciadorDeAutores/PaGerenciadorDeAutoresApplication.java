4ed006b770bf62bc8dbac3cdbf7b0ad7e2b1920d
MiguelCarreiraa <miguelitocarreira@hotmail.com>
2026-03-19 11:38:25 -0300

    Model Autor

---
+++ src/main/java/com/Carreira/PA_GerenciadorDeAutores/PaGerenciadorDeAutoresApplication.java
@@ -0,0 +1,13 @@
+package com.Carreira.PA_GerenciadorDeAutores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaGerenciadorDeAutoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaGerenciadorDeAutoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Carreira/PA_GerenciadorDeAutores/PaGerenciadorDeAutoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Carreira.PA_GerenciadorDeAutores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaGerenciadorDeAutoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
